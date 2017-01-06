197ef94eeba537a951a576fb87f5bf29a36c3695
KeitaNishimoto <k.mail.09070876640@icloud.com>
2017-01-07 02:35:34 +0900

    modify #12 LambdaExecutionContextに関数型の値を追加

--- types.d.ts
+++ types.d.ts
@@ -1,11 +1,15 @@
 export interface LambdaExecutionContext {
-    callbackWaitsForEmptyEventLoop: boolean
-    logGroupName:                   string
-    logStreamName:                  string
-    functionName:                   string
-    memoryLimitInMB:                string
-    functionVersion:                string
-    invokeid:                       string
-    awsRequestId:                   string
-    invokedFunctionArn:             string
+  callbackWaitsForEmptyEventLoop: boolean
+  done:                           Function
+  succeed:                        Function
+  fail:                           Function
+  logGroupName:                   string
+  logStreamName:                  string
+  functionName:                   string
+  memoryLimitInMB:                string
+  functionVersion:                string
+  getRemainingTimeInMillis:       Function
+  invokeid:                       string
+  awsRequestId:                   string
+  invokedFunctionArn:             string
 }
