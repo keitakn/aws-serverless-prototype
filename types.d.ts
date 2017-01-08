1f669a6cd56a98abca11c6af90f4ecfb45eaa178
KeitaNishimoto <k.mail.09070876640@icloud.com>
2017-01-08 23:50:33 +0900

    modify #12 LambdaEventのinterfaceが提供されていないので分かる範囲で自作定義

--- types.d.ts
+++ types.d.ts
@@ -1,15 +1,44 @@
-export interface LambdaExecutionContext {
-  callbackWaitsForEmptyEventLoop: boolean
-  done:                           Function
-  succeed:                        Function
-  fail:                           Function
-  logGroupName:                   string
-  logStreamName:                  string
-  functionName:                   string
-  memoryLimitInMB:                string
-  functionVersion:                string
-  getRemainingTimeInMillis:       Function
-  invokeid:                       string
-  awsRequestId:                   string
-  invokedFunctionArn:             string
+/**
+ * aws-lambdaのeventインターフェース
+ *
+ * "@types/aws-lambda"から正式な型定義が提供されるまでの仮対応
+ *
+ * @author keita-nishimoto
+ * @since 2016-01-09
+ */
+export interface LambdaExecutionEvent {
+  resource: string,
+  path: string,
+  httpMethod: string,
+  headers: any,
+  queryStringParameters: any,
+  pathParameters: any,
+  stageVariables: any,
+  requestContext: {
+    accountId: string,
+    resourceId: string,
+    stage: string,
+    requestId: string,
+    identity: {
+      cognitoIdentityPoolId: string,
+      accountId: string,
+      cognitoIdentityId: string,
+      caller: string,
+      apiKey: string,
+      sourceIp: string,
+      accessKey: null,
+      cognitoAuthenticationType: string,
+      cognitoAuthenticationProvider: string,
+      userArn: string,
+      userAgent: string,
+      user: string
+    },
+    resourcePath: string,
+    httpMethod: string,
+    apiId: string
+  },
+  body: any,
+  // TODO methodArnは特定の条件下でのみ設定されるようなので設定条件を調査し修正する
+  methodArn: string,
+  isBase64Encoded: boolean
 }
