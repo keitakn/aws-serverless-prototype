e4cba76dc972e52c228fd910d741f880d496089e
keita-koga <keita-koga@bm-sms.co.jp>
2017-02-03 12:03:15 +0900

    add #46 DynamoDbのResponseObjectを定義

---
+++ src/repositories/DynamoDbResponse.ts
@@ -0,0 +1,38 @@
+/**
+ * DynamoDbResponse
+ *
+ * @author keita-nishimoto
+ * @since 2016-02-03
+ */
+export namespace DynamoDbResponse {
+
+  /**
+   * ClientsテーブルのResponse
+   */
+  export interface Client {
+    Item: {
+      id: string;
+      secret: string;
+      name: string;
+      redirect_uri: string;
+      created_at: number;
+      updated_at: number;
+    }
+  }
+
+  /**
+   * UsersテーブルのResponse
+   */
+  export interface User {
+    Item: {
+      id: string;
+      email: string;
+      email_verified: number;
+      name: string;
+      gender: string;
+      birthdate: string;
+      created_at: number;
+      updated_at: number;
+    }
+  }
+}
