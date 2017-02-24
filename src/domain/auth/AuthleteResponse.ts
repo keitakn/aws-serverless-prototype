0fb8fe913bc95c378460d08c8f2ed3e7627c5e7f
keita-koga <keita-koga@bm-sms.co.jp>
2017-02-24 14:35:57 +0900

    modify #73 IntrospectionResponseをAuthleteResponse内に統合

--- src/domain/auth/AuthleteResponse.ts
+++ src/domain/auth/AuthleteResponse.ts
@@ -7,6 +7,67 @@
 export namespace AuthleteResponse
 {
   /**
+   * Introspectionレスポンスで返すべきHTTPステータスが設定される
+   */
+  type IntrospectionActions = "OK" | "BAD_REQUEST" | "FORBIDDEN" | "UNAUTHORIZED" | "INTERNAL_SERVER_ERROR";
+
+  /**
+   * IntrospectionResponse
+   * AuthleteのイントロスペクションAPIの結果
+   *
+   * @author keita-nishimoto
+   * @since 2017-01-25
+   */
+  export interface IntrospectionResponse {
+    /**
+     * イントロスペクションAPIの結果コード
+     */
+    resultCode: string;
+
+    /**
+     * イントロスペクションAPIの結果メッセージ
+     */
+    resultMessage: string;
+
+    /**
+     * レスポンスで返すべきHTTPステータスが設定される
+     * 設定される可能性があるのは以下の値
+     *
+     * - OK
+     * - BAD_REQUEST
+     * - FORBIDDEN
+     * - UNAUTHORIZED
+     * - INTERNAL_SERVER_ERROR
+     */
+    action: IntrospectionActions;
+
+    /**
+     * アクセストークンに紐付いているクライアントID
+     * 有効でないアクセストークンをリクエストした場合0が設定される
+     */
+    clientId: number;
+
+    /**
+     * レスポンスの詳細
+     */
+    responseContent: string;
+
+    /**
+     * アクセストークンが利用出来るスコープ
+     * 以下のような配列が設定される
+     *
+     * ['email', 'address']
+     */
+    scopes?: [string];
+
+    /**
+     * アクセストークンに紐付いているユーザーの識別子
+     * クライアントクレデンシャル等、アクセストークンにユーザーが紐付かない場合はこの値は設定されない
+     */
+    subject?: string;
+  }
+
+  /**
    * actionのデータ型（/auth/authorization API）
    * レスポンスで返すべきHTTPステータスが設定される
    */
