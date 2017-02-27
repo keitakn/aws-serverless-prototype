8dbf99f887bb788d93a9cf9ee05ba131341b2a7f
keita-koga <keita-koga@bm-sms.co.jp>
2017-02-27 18:44:15 +0900

    add #54 テストフレームワークをインストールし動作確認を行った

--- paths.js
+++ paths.js
@@ -12,6 +12,8 @@ const nodePaths = (process.env.NODE_PATH || '')
 module.exports = {
   appBuild: resolveApp('.webpack'),
   appFunctions: resolveApp('src/functions'),
+  integrationAuthTests: resolveApp('src/tests/integration/functions/auth'),
+  integrationTokenTests: resolveApp('src/tests/integration/functions/token'),
   appPackageJson: resolveApp('package.json'),
   appNodeModules: resolveApp('node_modules'),
   nodePaths: nodePaths
