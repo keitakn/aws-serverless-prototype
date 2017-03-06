7c19b30cc30bfb752732b943de542892894eed3a
keita-koga <keita-koga@bm-sms.co.jp>
2017-03-06 18:43:31 +0900

    add #84 正常系の最低限動くテストコードを作成

--- src/tests/integration/functions/auth/Authentication.test.ts
+++ src/tests/integration/functions/auth/Authentication.test.ts
@@ -1,11 +1,67 @@
-import * as mocha from "mocha";
-import * as chai from "chai";
+import {assert} from "chai";
+import {AuthApi} from "../../../lib/AuthApi";
+import AccessTokenRepository from "../../../../repositories/AccessTokenRepository";
+import AccessTokenEntity from "../../../../domain/auth/AccessTokenEntity";
+import {UserApi} from "../../../lib/UserApi";
 
-// TODO 動作確認の為だけの仮コミットなので後でちゃんと実装。 @keita-nishimoto
+/**
+ * 認証のテスト
+ */
 describe("Authentication", () => {
-  it("testSuccess", () => {
-    mocha.toString();
 
-    chai.assert.equal(123, 123);
+  /**
+   * テストに使うアクセストークン
+   */
+  let accessToken: string;
+
+  /**
+   * 事前にトークンを取得する
+   */
+  beforeEach((done: Function) => {
+    const request: AuthApi.IssueAccessTokenInCheatApiRequest = {
+      grantType: AuthApi.GrantTypesEnum.CLIENT_CREDENTIALS,
+      clientId: 1957483863470,
+      scopes: ["prototype_users", "prototype_clients"]
+    };
+
+    AuthApi.ApiClient.issueAccessTokenInCheatApi(request).then((response) => {
+      const accessTokenRepository = new AccessTokenRepository();
+      return accessTokenRepository.fetch(response.accessToken);
+    }).then((accessTokenEntity: AccessTokenEntity) => {
+      accessToken = accessTokenEntity.token;
+      console.log(accessTokenEntity);
+      done();
+    });
+  });
+
+  /**
+   * 正常系のテストケース
+   */
+  it("testSuccess", (done: Function) => {
+    const createUserRequest = {
+      email: "keita@gmail.com",
+      password: "password1234",
+      name: "keita",
+      gender: "male",
+      birthdate: "1990-01-01"
+    };
+
+    UserApi.ApiClient
+      .create(createUserRequest, accessToken)
+      .then((response) => {
+        const userId   = response.data.id;
+        const password = createUserRequest.password;
+
+        const request = {
+          id: userId,
+          password: password
+        };
+
+        return AuthApi.ApiClient.authentication(request);
+      })
+      .then((response) => {
+        assert.equal(response.data.authenticated, true);
+        done();
+      });
   });
 });
