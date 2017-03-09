cdb0084260bd868aa487cc61e334270c63a71220
keita-koga <keita-koga@bm-sms.co.jp>
2017-03-09 11:33:52 +0900

    add 認可コードフローで発行したアクセストークンを用いた正常系のケースを追加

--- src/tests/integration/functions/auth/Authorization.test.ts
+++ src/tests/integration/functions/auth/Authorization.test.ts
@@ -6,6 +6,8 @@ import {ClientApi} from "../../../lib/ClientApi";
 
 /**
  * 認可のテスト
+ *
+ * このテストを成功させるには事前にAuthleteにクライアントIDの登録が必要です。
  */
 describe("Authorization", () => {
 
@@ -14,20 +16,43 @@ describe("Authorization", () => {
    * クライアントクレデンシャルで発行したアクセストークンでAPIを呼び出せる事
    */
   it("testSuccessClientCredentials", () => {
-    const request: AuthApi.IssueAccessTokenInCheatApiRequest = {
+    const tokenRequest: AuthApi.IssueAccessTokenInCheatApiRequest = {
       grantType: AuthApi.GrantTypesEnum.CLIENT_CREDENTIALS,
       clientId: 1957483863470,
       scopes: ["prototype_clients"]
     };
 
-    return AuthApi.ApiClient.issueAccessTokenInCheatApi(request).then((response) => {
+    return AuthApi.ApiClient.issueAccessTokenInCheatApi(tokenRequest).then((response) => {
       const accessTokenRepository = new AccessTokenRepository();
       return accessTokenRepository.fetch(response.accessToken);
     }).then((accessTokenEntity: AccessTokenEntity) => {
-      return ClientApi.ApiClient.find(request.clientId, accessTokenEntity.token);
+      return ClientApi.ApiClient.find(tokenRequest.clientId, accessTokenEntity.token);
     }).then((response) => {
       assert.equal(response.status, 200);
-      assert.equal(response.data.client_id, request.clientId);
+      assert.equal(response.data.client_id, tokenRequest.clientId);
+    });
+  });
+
+  /**
+   * 正常系のテストケース
+   * 認可コードで発行したアクセストークンでAPIを呼び出せる事
+   */
+  it("testSuccessAuthorizationCode", () => {
+    const tokenRequest: AuthApi.IssueAccessTokenInCheatApiRequest = {
+      grantType: AuthApi.GrantTypesEnum.AUTHORIZATION_CODE,
+      clientId: 1957483863470,
+      subject: "796c6536-5e55-4da6-adf1-9a6badfb2e3c",
+      scopes: ["prototype_clients"]
+    };
+
+    return AuthApi.ApiClient.issueAccessTokenInCheatApi(tokenRequest).then((response) => {
+      const accessTokenRepository = new AccessTokenRepository();
+      return accessTokenRepository.fetch(response.accessToken);
+    }).then((accessTokenEntity: AccessTokenEntity) => {
+      return ClientApi.ApiClient.find(tokenRequest.clientId, accessTokenEntity.token);
+    }).then((response) => {
+      assert.equal(response.status, 200);
+      assert.equal(response.data.client_id, tokenRequest.clientId);
     });
   });
 });
