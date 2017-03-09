998ea358962afe540c3566fd8c256d636e551d99
keita-koga <keita-koga@bm-sms.co.jp>
2017-03-09 18:23:54 +0900

    modify 全体をtry catchで囲い、明示的にthrowしていた例外はreturn Promise.reject() を使うように変更

--- src/repositories/AccessTokenRepository.ts
+++ src/repositories/AccessTokenRepository.ts
@@ -25,40 +25,45 @@ export default class AccessTokenRepository implements AccessTokenRepositoryInter
    * @returns {Promise<AccessTokenEntity>}
    */
   async fetch(accessToken: string): Promise<AccessTokenEntity> {
-    const headers = {
-      "Content-Type": "application/json"
-    };
+    try {
+      const headers = {
+        "Content-Type": "application/json"
+      };
 
-    const requestData = {
-      token: accessToken
-    };
+      const requestData = {
+        token: accessToken
+      };
 
-    const requestConfig = {
-      headers: headers,
-      auth: {
-        username: Authlete.getApiKey(),
-        password: Authlete.getApiSecret()
+      const requestConfig = {
+        headers: headers,
+        auth: {
+          username: Authlete.getApiKey(),
+          password: Authlete.getApiSecret()
+        }
+      };
+
+      const response: AxiosResponse = await axios.post(
+        "https://api.authlete.com/api/auth/introspection",
+        requestData,
+        requestConfig
+      );
+
+      if (response.status !== 200) {
+        Logger.critical(response);
+        return Promise.reject(new InternalServerError());
       }
-    };
 
-    const response: AxiosResponse = await axios.post(
-      "https://api.authlete.com/api/auth/introspection",
-      requestData,
-      requestConfig
-    );
+      const introspectionResponse: AuthleteResponse.IntrospectionResponse = response.data;
+      const accessTokenEntity = new AccessTokenEntity(
+        accessToken
+      );
+      accessTokenEntity.introspectionResponse = introspectionResponse;
 
-    if (response.status !== 200) {
-      Logger.critical(response);
-      throw new InternalServerError();
+      return accessTokenEntity;
+    } catch (error) {
+      Logger.critical(error);
+      return Promise.reject(error);
     }
-
-    const introspectionResponse: AuthleteResponse.IntrospectionResponse = response.data;
-    const accessTokenEntity = new AccessTokenEntity(
-      accessToken
-    );
-    accessTokenEntity.introspectionResponse = introspectionResponse;
-
-    return accessTokenEntity;
   }
 
   /**
@@ -69,49 +74,60 @@ export default class AccessTokenRepository implements AccessTokenRepositoryInter
    * @returns {Promise<AccessTokenEntity>}
    */
   async issue(authorizationCode: string, redirectUri: string): Promise<AccessTokenEntity> {
-    const headers = {
-      "Content-Type": "application/json"
-    };
+    try {
+      const headers = {
+        "Content-Type": "application/json"
+      };
 
-    const requestData = {
-      parameters: `code=${authorizationCode}&grant_type=authorization_code&redirect_uri=${redirectUri}`
-    };
+      const requestData = {
+        parameters: `code=${authorizationCode}&grant_type=authorization_code&redirect_uri=${redirectUri}`
+      };
 
-    const requestConfig = {
-      headers: headers,
-      auth: {
-        username: Authlete.getApiKey(),
-        password: Authlete.getApiSecret()
+      const requestConfig = {
+        headers: headers,
+        auth: {
+          username: Authlete.getApiKey(),
+          password: Authlete.getApiSecret()
+        }
+      };
+
+      const response: AxiosResponse = await axios.post(
+        "https://api.authlete.com/api/auth/token",
+        requestData,
+        requestConfig
+      );
+
+      if (response.status !== 200) {
+        console.error(response);
+        return Promise.reject(new InternalServerError());
       }
-    };
 
-    const response: AxiosResponse = await axios.post(
-      "https://api.authlete.com/api/auth/token",
-      requestData,
-      requestConfig
-    );
+      const tokenResponse: AuthleteResponse.TokenResponse = response.data;
+      const accessTokenEntity = new AccessTokenEntity(tokenResponse.accessToken);
+      accessTokenEntity.tokenResponse = tokenResponse;
 
-    if (response.status !== 200) {
-      console.error(response);
-      throw new InternalServerError();
-    }
-
-    const tokenResponse: AuthleteResponse.TokenResponse = response.data;
-    const accessTokenEntity = new AccessTokenEntity(tokenResponse.accessToken);
-    accessTokenEntity.tokenResponse = tokenResponse;
-
-    if (accessTokenEntity.extractTokenAction() !== "OK") {
-      switch (accessTokenEntity.extractTokenAction()) {
-        case "BAD_REQUEST":
-          throw new BadRequestError(accessTokenEntity.tokenResponse.resultMessage);
-        case "FORBIDDEN":
-          throw new ForbiddenError(accessTokenEntity.tokenResponse.resultMessage);
-        default:
-          Logger.critical(accessTokenEntity.tokenResponse);
-          throw new InternalServerError(accessTokenEntity.tokenResponse.resultMessage);
+      if (accessTokenEntity.extractTokenAction() !== "OK") {
+        switch (accessTokenEntity.extractTokenAction()) {
+          case "BAD_REQUEST":
+            return Promise.reject(
+              new BadRequestError(accessTokenEntity.tokenResponse.resultMessage)
+            );
+          case "FORBIDDEN":
+            return Promise.reject(
+              new ForbiddenError(accessTokenEntity.tokenResponse.resultMessage)
+            );
+          default:
+            Logger.critical(accessTokenEntity.tokenResponse);
+            return Promise.reject(
+              new InternalServerError(accessTokenEntity.tokenResponse.resultMessage)
+            );
+        }
       }
-    }
 
-    return accessTokenEntity;
+      return accessTokenEntity;
+    } catch (error) {
+      Logger.critical(error);
+      return Promise.reject(error);
+    }
   }
 }
