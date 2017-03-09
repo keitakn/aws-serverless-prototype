b6fa79d0a8904722f38b9ba166cd44242a4b26d7
keita-koga <keita-koga@bm-sms.co.jp>
2017-03-09 14:08:06 +0900

    add testSuccessScopeByLimitDoseNotExist

--- src/tests/integration/functions/auth/Authorization.test.ts
+++ src/tests/integration/functions/auth/Authorization.test.ts
@@ -3,6 +3,7 @@ import {AuthApi} from "../../../lib/AuthApi";
 import AccessTokenRepository from "../../../../repositories/AccessTokenRepository";
 import AccessTokenEntity from "../../../../domain/auth/AccessTokenEntity";
 import {ClientApi} from "../../../lib/ClientApi";
+import {ResourceApi} from "../../../lib/ResourceApi";
 
 /**
  * 認可のテスト
@@ -10,6 +11,20 @@ import {ClientApi} from "../../../lib/ClientApi";
  * このテストを成功させるには事前にAuthleteにクライアントIDの登録が必要です。
  */
 describe("Authorization", () => {
+
+  /**
+   * 事後処理でテスト用に削除したリソースを再登録する
+   */
+  afterEach(() => {
+    const createRequest: ResourceApi.CreateRequest = {
+      http_method: "GET",
+      resource_path: "clients",
+      name: "Find Client.",
+      scopes: ["prototype_clients"]
+    };
+
+    return ResourceApi.ApiClient.create(createRequest);
+  });
 
   /**
    * 正常系のテストケース
@@ -51,6 +66,32 @@ describe("Authorization", () => {
     }).then((accessTokenEntity: AccessTokenEntity) => {
       return ClientApi.ApiClient.find(tokenRequest.clientId, accessTokenEntity.token);
     }).then((response) => {
+      assert.equal(response.status, 200);
+      assert.equal(response.data.client_id, tokenRequest.clientId);
+    });
+  });
+
+  /**
+   * 正常系のテストケース
+   * スコープを保持していないが、制限が存在しないので正常終了する
+   */
+  it("testSuccessScopeByLimitDoseNotExist", () => {
+    // あえてアクセスに必要なスコープをリクエストしない
+    const tokenRequest: AuthApi.IssueAccessTokenInCheatApiRequest = {
+      grantType: AuthApi.GrantTypesEnum.AUTHORIZATION_CODE,
+      clientId: 1957483863470,
+      subject: "796c6536-5e55-4da6-adf1-9a6badfb2e3c",
+      scopes: ["email"]
+    };
+
+    return (async () => {
+      const tokenCreateResponse = await AuthApi.ApiClient.issueAccessTokenInCheatApi(tokenRequest);
+
+      // DBからリソースを削除する
+      await ResourceApi.ApiClient.destroy("GET_clients");
+
+      return await ClientApi.ApiClient.find(tokenRequest.clientId, tokenCreateResponse.accessToken);
+    })().then((response) => {
       assert.equal(response.status, 200);
       assert.equal(response.data.client_id, tokenRequest.clientId);
     });
