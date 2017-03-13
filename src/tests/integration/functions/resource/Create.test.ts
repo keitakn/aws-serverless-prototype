00b20c54103f2accbadd815368961a1850310034
keita-koga <keita-koga@bm-sms.co.jp>
2017-03-13 16:33:45 +0900

    add リソース作成のテストコードを追加

---
+++ src/tests/integration/functions/resource/Create.test.ts
@@ -0,0 +1,46 @@
+import {assert} from "chai";
+import {ResourceApi} from "../../../lib/ResourceApi";
+
+/**
+ * リソース作成のテスト
+ */
+describe("CreateResource", () => {
+
+  /**
+   * 事前にリソース登録を行っておく
+   */
+  beforeEach(() => {
+    const resourceId = "POST_tests";
+
+    return (async () => {
+      const destroyResponse = await ResourceApi.ApiClient.destroy(resourceId);
+      assert.equal(destroyResponse.status, 204);
+    })();
+  });
+
+  /**
+   * 正常系のテストケース
+   */
+  it("testSuccessDestroy", () => {
+    const createRequest: ResourceRequest.CreateRequest = {
+      http_method: "POST",
+      resource_path: "tests",
+      name: "テストに利用するリソース",
+      scopes: ["test"]
+    };
+
+    return (async () => {
+      const createResourceResponse = await ResourceApi.ApiClient.create(createRequest);
+
+      assert.equal(createResourceResponse.status, 201);
+      assert.equal(createResourceResponse.data.id, "POST/tests");
+      assert.equal(createResourceResponse.data.http_method, createRequest.http_method);
+      assert.equal(createResourceResponse.data.resource_path, createRequest.resource_path);
+      assert.equal(createResourceResponse.data.name, createRequest.name);
+      assert.deepEqual(createResourceResponse.data.scopes, createRequest.scopes);
+
+      const findResourceResponse = await ResourceApi.ApiClient.find("POST_tests");
+      assert.equal(findResourceResponse.status, 200);
+    })();
+  });
+});
