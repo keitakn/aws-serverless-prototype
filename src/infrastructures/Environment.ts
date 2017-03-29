b0ec73861c00124dd386ac1ed9cc559188df9a37
keita-koga <keita-koga@bm-sms.co.jp>
2017-03-29 14:29:24 +0900

    modify #147 serverless-offlineのバージョンUPによって不要になったパラメータを削除

--- src/infrastructures/Environment.ts
+++ src/infrastructures/Environment.ts
@@ -1,33 +1,17 @@
-import {LambdaEvent} from "../types/aws/types";
-
 /**
  * Environment
  *
  * @author keita-nishimoto
  * @since 2017-02-01
  */
-export default class Environment<T extends LambdaEvent> {
-
-  /**
-   * constructor
-   */
-  constructor(private _event: T) {
-  }
-
-  /**
-   * @returns {T}
-   */
-  get event(): T {
-    return this._event;
-  }
+export default class Environment {
 
   /**
    * ローカル環境かどうかを判定する
    *
-   * @param event
    * @returns {boolean}
    */
-  isLocal(): boolean {
+  static isLocal(): boolean {
 
     if (process.env.IS_OFFLINE) {
       return true;
